8f273c0cf3161ca7ec1cf8434941b13a873628dd
Jacob Jahraus <jjahraus@mediaocean.com>
2018-08-03 12:08:54 -0500

    part6

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -22,7 +22,7 @@ class QuizQuestion extends Component{
     
     handleClick(buttonText){
         if(buttonText === this.props.quiz_question.answer){
-            this.props.showNextQuestionHandler;
+            this.props.showNextQuestionHandler();
         }
     }
 }
